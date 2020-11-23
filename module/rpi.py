59e43b3f41b06c9ee4ba33829837ab518feded16
JianwenLiu219 <jianwen_liu219@163.com>
2020-11-23 20:04:25 +0800

    修改计算

--- module/rpi.py
+++ module/rpi.py
@@ -171,7 +171,7 @@ class RPI:
         for i in range(0, 16):
             if i&1 :
                 index = ((data['seg'][i-1]&15)<<4) | (data['seg'][i]&15)
-                logger.error("index: " + data['seg'][i-1] + " " + data['seg'][i] + " " + str(index))
+                logger.error("index: " + str(data['seg'][i-1]) + " " + str(data['seg'][i]) + " " + str(index))
                 self.SEGState[i>>1] = index
                 # self.SEGState[i>>1] = random.randint(0, 8) % 8
             self.LEDState[i] = data['led'][i]
